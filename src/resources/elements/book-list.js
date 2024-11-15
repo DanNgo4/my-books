07827b8e551c5d18c791cb804cf6640565ca7a77
Dan Ngo <danngo.au@gmail.com>
2024-11-15 22:18:50 +1100

    up to 4.4 (page 93 slide 123)

--- src/resources/elements/book-list.js
+++ src/resources/elements/book-list.js
@@ -3,6 +3,13 @@ import { bindable } from "aurelia-framework";
 export class BookList {
   @bindable books;
 
+  bookLocation(isFirst, isLast) {
+    if (isFirst) return "- first book";
+    if (isLast) return "- last book";
+
+    return "";
+  }
+
   removeBook(index) {
     this.books.splice(index, 1);
   }
