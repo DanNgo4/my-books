23c631fc42d1f9be7fdc52e53549d01913238448
Dan Ngo <danngo.au@gmail.com>
2024-11-16 22:26:33 +1100

    up to 4.5 (page 98 slide 128)

--- src/resources/elements/books.js
+++ src/resources/elements/books.js
@@ -1,12 +1,24 @@
-import { inject } from "aurelia-framework";
+import { inject, bindable, computedFrom, observable } from "aurelia-framework";
 import { BookApi } from "../../services/book-api";
 
 @inject (BookApi)
 export class Books {
+  @observable bookTitle = ""; // extracts bookTitle to be defined outside of the constructor and marks it as @observable
+
   constructor (bookApi) {
     this.bookTitle = "";
     this.books = [];
     this.bookApi = bookApi;
+  }
+
+  @computedFrom("bookTitle.length") // indicates that the getter wants to be notified when the bookTitle.length value changes
+  get canAdd() {
+    return this.bookTitle.length === 0;
+  }
+
+  // defines a hook/subscriber method to be called whenever the value of bookTitle changes
+  bookTitleChanged(newValue, oldValue) {
+    console.log(`Book title changed, Old Value: ${oldValue}, New Value: ${newValue}`);
   }
 
   addBook() {
