d5fa92ad5b09c6904aa51f835a9361204534c77e
Dan Ngo <dan.ngo@onset.com.au>
2024-11-21 00:16:19 +1100

    up to 6.0 (page 119 slide 149)

--- src/resources/elements/books.js
+++ src/resources/elements/books.js
@@ -1,22 +1,37 @@
 import { inject, bindable, computedFrom, observable } from "aurelia-framework";
+import { BindingSignaler } from "aurelia-templating-resources";
+
 import { BookApi } from "../../services/book-api";
 
-@inject (BookApi)
+// injects the BookApi and BindingSignaler class from the Aurelia DI container, which then will take an instance of each class singleton
+@inject (BookApi, BindingSignaler)
 export class Books {
   @observable bookTitle = ""; // extracts bookTitle to be defined outside of the constructor and marks it as @observable
 
-  constructor (bookApi) {
+  constructor (bookApi, bindingSignaler) {
     this.bookTitle = "";
     this.books = [];
     this.bookApi = bookApi;
+
+    // alternative implementation with signal binding behaviour (bindingSignaler)
+    this.bindingSignaler = bindingSignaler;
   }
 
-  @computedFrom("bookTitle.length") // indicates that the getter wants to be notified when the bookTitle.length value changes
-  get canAdd() {
+  canAdd() {
     return this.bookTitle.length === 0;
   }
 
-  // defines a hook/subscriber method to be called whenever the value of bookTitle changes
+  refreshSignal() {
+    // fires a 'can-add-signal' event whenever the refreshSignal method is called
+    this.bindingSignaler.signal('can-add-signal');
+  }
+
+  /* @computedFrom("bookTitle.length") // indicates that the getter wants to be notified when the bookTitle.length value changes
+  get canAdd() {
+    return this.bookTitle.length === 0;
+  } */
+
+  // defines a hook/subscriber method by convention to be called whenever the value of bookTitle changes
   bookTitleChanged(newValue, oldValue) {
     console.log(`Book title changed, Old Value: ${oldValue}, New Value: ${newValue}`);
   }
