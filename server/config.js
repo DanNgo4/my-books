2778cf09b5d4f842d41136997f41c1f03817915b
Dan Ngo <dan.ngo@onset.com.au>
2024-12-03 21:23:09 +1100

    last config on Mac

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'secret'     : 'my-books-auth-key',
-    'database'   : 'mongodb://localhost/my-books',
-    'authEnabled': false
+    'secret'     : 'my-books-auth-key', // token based auth key
+    'database'   : 'mongodb://localhost/my-books',  // MongoDB database
+    'authEnabled': false    // toggle to enable or disable auth
 };
