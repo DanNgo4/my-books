76dc4fd509c721ee6dc4e64d1ce5691cb4fc974e
Dan Ngo <danngo.au@gmail.com>
2024-12-11 09:30:26 +1100

    up to 10.2.4 (page 252 slide 282)

--- client/src/resources/elements/login.js
+++ client/src/resources/elements/login.js
@@ -1,5 +1,25 @@
+import { inject } from "aurelia-framework";
+import { Router } from "aurelia-router";
+
+import { AuthService } from "../../services/auth-service";
+
+@inject(Router, AuthService)
 export class Login {
-  constructor() {
-    
+  constructor(router, authService) {
+    this.router = router;
+    this.authService = authService;
+  }
+
+  logIn() {
+    this.authService
+      .logIn(this.userName, this.password)
+      .then(tokenResult => {
+        if (tokenResult.success) {
+          this.errorMessage = "";
+          this.router.navigateToRoute("home");
+        } else {
+          this.errorMessage = tokenResult.message;
+        }
+      })
   }
 }
